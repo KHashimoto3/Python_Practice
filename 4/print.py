ba8a6df2068b78c469cda2f18fee313f9b9d2acb
KHashimoto3 <hashi.koki385@gmail.com>
2024-04-10 16:10:49 +0900

    いろいろな出力の方法を学んだ。

---
+++ 4/print.py
@@ -0,0 +1,12 @@
+# カンマ区切りの複数文字列出力
+print("baseball", "soccer", "basketball", "tennis")
+
+# 末尾に任意もの文字を付加して出力
+print("200", end="円")
+
+# 開業せずに出力
+print("あいうえお", end="")
+print("かきくけこ")
+
+# 区切り文字を指定して出力
+print("baseball", "soccer", "basketball", "tennis", sep=", ")
